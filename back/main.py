f48435496795bc699de51a3290dfd1595ca1d188
Jorian Gaurin <jorian.gaurin@etudiant.univ-lr.fr>
2024-07-09 03:14:48 +0200

    script convert YTB to audio, audio to text, speaker diarization

--- back/main.py
+++ back/main.py
@@ -1,21 +1,22 @@
 from download_youtube_audio import download_youtube_audio
-from transcribe_audio import transcribe_audio
+from transcribe_audio import transcribe_audio_with_timestamps
+from diarize_audio import diarize_audio
 
 def main():
 
     # URL de la vidéo YouTube
     youtube_url = "https://www.youtube.com/watch?v=OZXbiB0g950&t=619s&ab_channel=TocaToast"
 
-     # Dossier de sortie
+    # Dossier de sortie
     output_folder = "downloaded_audio"
 
     # On call la fonction pour télécharger l'audio
     # audio_file_path = download_youtube_audio(youtube_url, output_folder)
 
-    audio_file_path = "downloaded_audio\\audio.mp3"
+    audio_file_path = "downloaded_audio\\audio.wav"
 
     # Transcrire l'audio téléchargé
-    transcription = transcribe_audio(audio_file_path)
+    transcription = transcribe_audio_with_timestamps_and_diarization(audio_file_path, "example.srt")
     print("Transcription de l'audio :")
     print(transcription)
 
